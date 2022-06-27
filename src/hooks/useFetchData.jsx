ceadb9c522145b4e99f68500ba37c696c8e96f71
Luis Ocasio <luis.ocasio@wizehive.com>
2022-06-27 13:54:46 -0600

    added api to env

--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -1,9 +1,10 @@
 const useFetchData = async (searchMovie) => {
   try {
     let response = await fetch(
-      `http://www.omdbapi.com/?t=${searchMovie}&apikey=17279a41`
+      `http://www.omdbapi.com/?t=${searchMovie}&${import.meta.env.VITE_APIKEY}`
     );
     let data = await response.json();
+    console.log("data: ", data);
     return data;
   } catch (error) {
     console.error("Error: ", error);
